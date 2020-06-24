6bc3924724a1000214be2728dcd31fe7ca695f98
unknown <moreno7046@hotmail.com>
2020-06-24 14:54:02 -0500

    finished exercise 2

--- exercise2/statically.js
+++ exercise2/statically.js
@@ -1,23 +1,29 @@
 class Statically {
-    //fix this code, so all of the instantiation and console.logs() work.  Try not to change the signature of the class, unless absolutely necessary.
-    static #glue = "Epoxy";
-    jello = "Jello";
+  //fix this code, so all of the instantiation and console.logs() work.  Try not to change the signature of the class, unless absolutely necessary.
+  static #glue = 'Epoxy';
+  jello = 'Jello';
 
-    moveAndShake() {
-        console.log("Never stagnate in life.  Be agile, change, and improve...");
-    }
+  moveAndShake() {
+    console.log('Never stagnate in life.  Be agile, change, and improve...');
+  }
 
-    static stubborn() {
-        this.moveAndShake();
-        console.log("Please don't make me change... I just don't like it...");
-    }
+  static stubborn() {
+    this.prototype.moveAndShake();
+    console.log("Please don't make me change... I just don't like it...");
+  }
 
-    static explain() {
-        stubborn();
-        console.log(#glue);
-        //write an explanation for how the keyword static behaves differently in javascript vs. java.
-    }
+  static explain() {
+    this.stubborn();
+    console.log(this.#glue);
+    //write an explanation for how the keyword static behaves differently in javascript vs. java.
+    // in javascript you can call instance methods (using this.prototype) within a static method while in java you need to create an instance (object) first of the class and then...
+    // you can call the non-static method from the instance created.
+    // finally I created a static getter below at the end of this class so that I could console log the glue property.
+  }
 
+  static get glue() {
+    return this.#glue;
+  }
 }
 
 //get the below code to run correctly without errors:  Make sure you explain what you did in the the explain() method, defined in the class.  Do NOT remove the 'static' keyword from the explain() method.
@@ -25,10 +31,10 @@ class Statically {
 const stats = new Statically();
 
 stats.moveAndShake();
-stubborn();
-stats.explain();
+Statically.stubborn();
+Statically.explain();
 
-console.log(stats.#glue);
+console.log(Statically.glue);
 console.log(stats.jello);
 
 //Once you have finished getting statistically.js to run, refactor the code here in Java.
